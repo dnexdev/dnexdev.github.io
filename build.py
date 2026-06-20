27625c4a83524b52ecb038b4386ce26be28a302a
JustListenToTheSong <JustListenToTheSong@duck.com>
2026-06-19 23:04:12 -0400

    add rss

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import os
 import markdown2
+import shutil
 from jinja2 import Environment, FileSystemLoader
+from datetime import datetime, timezone
 
 env = Environment(loader=FileSystemLoader("./templates/"))
 
@@ -10,39 +12,60 @@ posts = "./posts"
 
 save_path = "./docs"
 
+SITE_URL = "https://dnex.dev"
+
 post_list = []
 
-for log in os.scandir(posts):
-    if log.is_file():
-        with open(log.path, "r") as f:
-            content = f.read()
-            arr = content.split("---")
+# Clean previous build
+if os.path.exists(save_path):
+    shutil.rmtree(save_path)
+os.makedirs(save_path)
 
-            frontmatter = arr[1].splitlines()[1::]
+for entry in os.scandir(posts):
+    if not entry.is_dir():
+        continue
+    
+    post_folder = entry.name
+    md_path = os.path.join(entry.path, "post.md")
 
-            title = frontmatter[0].split(":")[1].lstrip()[1:-1] 
-            # print(title)
-            date = frontmatter[1].split(":")[1].lstrip()
-            # print(date)
-            tags = eval(frontmatter[2].split(":")[1].lstrip())
-            # print(tags)
+    with open(md_path, "r") as f:
+        content = f.read()
+        arr = content.split("---")
 
-            blogtext = arr[2].lstrip()
-            output = markdown2.markdown(blogtext)
+        frontmatter = arr[1].splitlines()[1::]
 
-            filename = f"{date}-{title}.html"
-            completename = os.path.join(save_path, filename)
-            post_content = template.render(log_title=title, page=output)
+        title = frontmatter[0].split(":")[1].lstrip()[1:-1] 
+        date = frontmatter[1].split(":")[1].lstrip()
+        tags = eval(frontmatter[2].split(":")[1].lstrip())
 
-            with open(completename, mode="w", encoding="utf-8") as message:
-                message.write(post_content)
-            
-            post_list.append({
-                "title": title,
-                "date": date,
-                "tags": tags,
-                "url": f"{filename}"
-            })
+        blogtext = arr[2].lstrip()
+        output = markdown2.markdown(blogtext)
+    
+    out_post_dir = os.path.join(save_path, post_folder)
+    os.makedirs(out_post_dir, exist_ok=True)
+
+    for item in os.scandir(entry.path):
+        if item.name == "post.md":
+            continue
+        
+        src = item.path
+        dst = os.path.join(out_post_dir, item.name)
+        if item.is_dir():
+            shutil.copytree(src, dst, dirs_exist_ok=True)
+        else:
+            shutil.copy2(src, dst)
+
+    post_content = template.render(log_title=title, page=output)
+
+    with open(os.path.join(out_post_dir, "index.html"), mode="w", encoding="utf-8") as message:
+        message.write(post_content)
+    
+    post_list.append({
+        "title": title,
+        "date": date,
+        "tags": tags,
+        "url": f"{post_folder}/"
+    })
 
 post_list.sort(key=lambda p: p["date"], reverse=True)
 
@@ -51,3 +74,16 @@ index_content = index_template.render(posts=post_list)
 
 with open(os.path.join(save_path, "index.html"), mode="w", encoding="utf-8") as f:
     f.write(index_content)
+
+if os.path.exists("./static"):
+    shutil.copytree("./static", os.path.join(save_path, "static"), dirs_exist_ok=True)
+
+atom_template = env.get_template("atom.xml")
+atom_content = atom_template.render(
+    posts=post_list,
+    site_url=SITE_URL,
+    updated=datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
+)
+
+with open(os.path.join(save_path, "atom.xml"), "w", encoding="utf-8") as f:
+    f.write(atom_content)
